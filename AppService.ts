f0af813f076f4d070e941b0feda976d1c0fced4a
rpm42 <rpm42@ya.ru>
2023-05-16 13:34:53 +0300

    1 file modified

--- AppService.ts
+++ AppService.ts
@@ -5,39 +5,6 @@ import KeySubject from './utils/KeySubject'
 import ValueSubject from './utils/ValueSubject'
 import { map } from 'rxjs/operators'
 
-export class FontService {
-  fontTheme$ = new KeySubject<FontTheme>('roboto', FONT_THEMES)
-  bodyFamily$ = new ValueSubject<string>()
-  headerFamily$ = new ValueSubject<string>()
-  bodyWeight$ = new ValueSubject<number>()
-  boldWeight$ = new ValueSubject<number>()
-  headerWeight$ = new ValueSubject<number>()
-  bodySize$ = new ValueSubject<number>()
-
-  constructor() {
-    this.fontTheme$.value$
-      .pipe(map(v => v.bodyFamily))
-      .subscribe(this.bodyFamily$)
-    this.fontTheme$.value$
-      .pipe(map(v => v.headerFamily))
-      .subscribe(this.headerFamily$)
-    this.fontTheme$.value$
-      .pipe(map(v => v.bodyWeight))
-      .subscribe(this.bodyWeight$)
-    this.fontTheme$.value$
-      .pipe(map(v => v.boldWeight))
-      .subscribe(this.boldWeight$)
-    this.fontTheme$.value$
-      .pipe(map(v => v.headerWeight))
-      .subscribe(this.headerWeight$)
-    this.fontTheme$.value$
-      .pipe(map(v => v.bodySize))
-      .subscribe(this.bodySize$)
-    this.fontTheme$.next('roboto')
-  }
-  init() {}
-}
-
 export default class AppService {
   colorTheme$ = new KeySubject<ColorTheme>('light', COLOR_THEMES)
   background$ = new ValueSubject<string>()
